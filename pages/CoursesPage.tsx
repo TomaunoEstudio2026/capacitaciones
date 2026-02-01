09dc4b57144d63f777786e922243e48462352ca2
Javier Móttola <fotografiasanjorge2022@gmail.com>
2026-02-01 07:58:55 -0300

    Update redirect URL in handleInscribirme function

--- pages/CoursesPage.tsx
+++ pages/CoursesPage.tsx
@@ -158,7 +158,7 @@ const CoursesPage: React.FC = () => {
   };
 
   const handleInscribirme = () => {
-    window.location.href = 'altacursos/altacursos.html';
+    window.location.href = 'https://formularioinscripcion.vercel.app';
   };
 
   return (
